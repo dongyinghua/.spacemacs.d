493d87d2767fdb168f43ee4de41a946697336157
dongyinghua <dlut_dyh@163.com>
2022-03-07 09:40:11 +0800

    添加了对python的配置

--- init-org.el
+++ init-org.el
@@ -1,63 +1,74 @@
-;;org
+;;org相关配置
 (defun open-my-init-org ()
   (interactive)
   (find-file "~/.spacemacs.d/init-org.el"))
 (global-set-key (kbd "<f2>") 'open-my-init-org)
 
+;;Important Note by Spacemacs
 (with-eval-after-load 'org
   (add-to-list 'org-export-backends 'md)
-  (setq org-startup-indented t))
+  (setq org-startup-indented t)
+  ;;Different bullets. By default the list is set to ("◉" "○" "✸" "✿").
+  (setq org-bullets-bullet-list '("■" "◆" "▲" "▶"))
 
-;;org-roam
-;;必须要有init.el中加入(require 'org-roam-protocol)以及打开Emacs Server，否则网页抓取会出现问题
-;;org-roam的网页抓取原理是利用 org-protocol 这样的外部程序和 Emacs 进行通信的机制
-(require 'org-roam-protocol)
-(setq org-roam-capture-templates
-  '(
-     ("d" "default" plain "%?"
-       :target (file+head "%<%Y%m%d%H>_${slug}.org"
-                 "#+latex_header:\n#+title: ${title}\n#+roam_alias:\n#+roam_key:\n#+roam_tags:\n\n")
-       :unnarrowed t)
-     ("p" "Paper Note" plain "* FIRST PASS\n\n ** Category\n\n** Context\n\n** Correctness\n\n** Contribution\n\n** Clarity\n * SECOND PASS\n\n* * THIRD PASS"
-       :target (file+head "%<%Y%m%d%H>_${slug}.org"
-                 "#+latex_header:\n#+title: ${title}\n#+roam_alias:\n#+roam_key:\n#+roam_tags:\n\n")
-       :unnarrowed t
+  ;;org-mode for GTD
+  (with-eval-after-load 'org-agenda
+    (setq org-agenda-files "/Users/yinghuadong/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/inbox.org")
+    (require 'org-projectile)
+    (push (org-projectile:todo-files) org-agenda-files))
+
+  ;;org-roam
+  ;;必须要有init.el中加入(require 'org-roam-protocol)以及打开Emacs Server，否则网页抓取会出现问题
+  ;;org-roam的网页抓取原理是利用 org-protocol 这样的外部程序和 Emacs 进行通信的机制
+  (require 'org-roam-protocol)
+  (setq org-roam-capture-templates
+    '(
+       ("d" "default" plain "%?"
+         :target (file+head "%<%Y%m%d%H>_${slug}.org"
+                   "#+latex_header:\n#+title: ${title}\n#+roam_alias:\n#+roam_key:\n#+roam_tags:\n\n")
+         :unnarrowed t)
+       ("p" "Paper Note" plain "* FIRST PASS\n\n ** Category\n\n** Context\n\n** Correctness\n\n** Contribution\n\n** Clarity\n * SECOND PASS\n\n* * THIRD PASS"
+         :target (file+head "%<%Y%m%d%H>_${slug}.org"
+                   "#+latex_header:\n#+title: ${title}\n#+roam_alias:\n#+roam_key:\n#+roam_tags:\n#+keywords:\n\n")
+         :unnarrowed t
+         )
        )
-     )
-  )
+    )
 
-;;网页抓取
-(setq org-roam-capture-ref-templates
-  '(
-     ("a" "Annotation" plain
-       "%U ${body}\n"
-       :target (file+head "${slug}.org"
-                 "#+title: ${title}\n#+roam_key: ${ref}\n#+roam_alias:\n#+roam_tags:\n\n")
-       ;; :immediate-finish t
-       :unnarrowed t
+  ;;网页抓取
+  (setq org-roam-capture-ref-templates
+    '(
+       ("a" "Annotation" plain
+         "%U ${body}\n"
+         :target (file+head "${slug}.org"
+                   "#+title: ${title}\n#+roam_key: ${ref}\n#+roam_alias:\n#+roam_tags:\n\n")
+         ;; :immediate-finish t
+         :unnarrowed t
+         )
+       ("r" "ref" plain "* FIRST PASS\n\n** Category\n\n** Context\n\n** Correctness\n\n** Contribution\n\n** Clarity\n\n* SECOND PASS\n\n* THIRD PASS\n\n"
+         :target (file+head "${slug}.org"
+                   "#+title: ${title}\n#+roam_key: ${ref}\n#+roam_alias:\n#+roam_tags:\n\n")
+         :unnarrowed t)
        )
-     ("r" "ref" plain "* FIRST PASS\n\n** Category\n\n** Context\n\n** Correctness\n\n** Contribution\n\n** Clarity\n\n* SECOND PASS\n\n* THIRD PASS\n\n"
-       :target (file+head "${slug}.org"
-                 "#+title: ${title}\n#+roam_key: ${ref}\n#+roam_alias:\n#+roam_tags:\n\n")
-       :unnarrowed t)
-     )
-  )
+    )
 
-(use-package org-roam-ui
-  :after org-roam
-  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-  ;;         a hookable mode anymore, you're advised to pick something yourself
-  ;;         if you don't care about startup time, use
-  ;;  :hook (after-init . org-roam-ui-mode)
-  :config
-  (setq org-roam-ui-sync-theme t
-    org-roam-ui-follow t
-    org-roam-ui-update-on-save t
-    org-roam-ui-open-on-start t)
-  )
+  (use-package org-roam-ui
+    :after org-roam
+    ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+    ;;         a hookable mode anymore, you're advised to pick something yourself
+    ;;         if you don't care about startup time, use
+    ;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+      org-roam-ui-follow t
+      org-roam-ui-update-on-save t
+      org-roam-ui-open-on-start t)
+    )
 
-(use-package org-roam-bibtex
-  :after org-roam
-  :hook (org-roam-mode . org-roam-bibtex-mode)
-  :bind (:map org-mode-map
-          (("C-c n a" . orb-note-actions))))
+  (use-package org-roam-bibtex
+    :after org-roam
+    :hook (org-roam-mode . org-roam-bibtex-mode)
+    :bind (:map org-mode-map
+            (("C-c n a" . orb-note-actions))))
+
+  )
--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; -*- mode: emacs-lisp; lexical-binding: t -*-
+
 ;; This file is loaded by Spacemacs at startup.
 ;; It must be stored in your home directory.
 
@@ -32,7 +32,8 @@ This function should only modify configuration layer settings."
 
     ;; List of configuration layers to load.
     dotspacemacs-configuration-layers
-    '(javascript
+    '(
+       javascript
        ;; ----------------------------------------------------------------
        ;; Example of useful layers you may want to use right away.
        ;; Uncomment some layer names and press `SPC f e R' (Vim style) or
@@ -59,7 +60,8 @@ This function should only modify configuration layer settings."
          org-enable-roam-server t
          org-enable-roam-protocol t
          org-roam-directory "~/Documents/org-roam-directory"
-         org-roam-v2-ack t)
+         org-roam-v2-ack t
+         org-projectile-file "TODOs.org")
        (shell :variables
          shell-default-height 30
          shell-default-position 'bottom)
@@ -78,6 +80,14 @@ This function should only modify configuration layer settings."
        (latex :variables
          latex-build-engine 'xetex
          latex-enable-magic t)
+       (python :variables
+         python-backend 'lsp
+         python-test-runner 'pytest
+         python-formatter 'yapf
+         python-format-on-save t
+         python-save-before-test nil
+         python-sort-imports-on-save t
+         )
        )
 
 
@@ -646,6 +656,12 @@ before packages are loaded."
 
   ;;快捷键绑定
   (load-file "~/.spacemacs.d/init-keybindings.el")
+
+  ;;python
+  (add-to-list 'spacemacs--python-pyenv-modes 'pyenv)
+  (setq python-indent-guess-indent-offset-verbose nil)
+ (add-to-list 'spacemacs--python-pyvenv-modes 'pyvenv)
+  (setq pyvenv-workon '("~/.pyenv/versions"))
   )
 
 
@@ -661,8 +677,7 @@ This function is called at the very end of Spacemacs initialization."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-    '(web-beautify tern prettier-js npm-mode nodejs-repl livid-mode skewer-mode js2-refactor multiple-cursors js2-mode js-doc import-js grizzl impatient-mode helm-gtags ggtags dap-mode bui counsel-gtags counsel swiper ivy add-node-modules-path zotxt zonokai-emacs zenburn-theme zen-and-art-theme youdao-dictionary yasnippet-snippets ws-butler writeroom-mode winum white-sand-theme which-key vterm volatile-highlights vi-tilde-fringe uuidgen use-package unicode-fonts unfill undo-tree underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil treemacs-all-the-icons toxi-theme toc-org terminal-here tao-theme tangotango-theme tango-plus-theme tango-2-theme symon symbol-overlay sunny-day-theme sublime-themes subatomic256-theme subatomic-theme string-inflection string-edit spaceline-all-the-icons spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle shell-pop seti-theme reverse-theme restart-emacs rebecca-theme rainbow-delimiters railscasts-theme quickrun pyim purple-haze-theme professional-theme popwin planet-theme phoenix-dark-pink-theme phoenix-dark-mono-theme password-generator paradox pangu-spacing overseer orgit-forge organic-green-theme org-superstar org-roam-ui org-roam-bibtex org-rich-yank org-projectile org-present org-pomodoro org-mime org-download org-contrib org-cliplink open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme noctilux-theme naquadah-theme nameless mwim mustang-theme multi-term multi-line monokai-theme monochrome-theme molokai-theme moe-theme modus-themes mmm-mode minimal-theme material-theme markdown-toc majapahit-theme magit-todos magit-svn magit-gitflow magit-delta madhat2r-theme macrostep lush-theme lsp-ui lsp-treemacs lsp-origami lorem-ipsum link-hint light-soap-theme kaolin-themes jbeans-theme jazz-theme ir-black-theme inspector inkpot-theme info+ indent-guide hybrid-mode hungry-delete htmlize highlight-parentheses highlight-numbers highlight-indentation heroku-theme hemisu-theme helm-xref helm-themes helm-swoop helm-purpose helm-projectile helm-org-rifle helm-org helm-mode-manager helm-make helm-lsp helm-ls-git helm-git-grep helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag hc-zenburn-theme gruvbox-theme gruber-darker-theme grandshell-theme gotham-theme google-translate golden-ratio gnuplot gitignore-templates git-timemachine git-modes git-messenger git-link git-gutter-fringe gh-md gandalf-theme fuzzy font-lock+ flyspell-correct-helm flycheck-pos-tip flycheck-package flycheck-elsa flx-ido flatui-theme flatland-theme find-by-pinyin-dired farmhouse-theme fancy-battery eziam-theme eyebrowse expand-region exotica-theme evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-textobj-line evil-surround evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-ediff evil-easymotion evil-collection evil-cleverparens evil-args evil-anzu eval-sexp-fu espresso-theme eshell-z eshell-prompt-extras esh-help emr elisp-slime-nav editorconfig dumb-jump drag-stuff dracula-theme dotenv-mode doom-themes django-theme dired-quick-sort diminish devdocs define-word darktooth-theme darkokai-theme darkmine-theme darkburn-theme dakrone-theme cyberpunk-theme column-enforce-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized clues-theme clean-aindent-mode chocolate-theme chinese-conv cherry-blossom-theme centered-cursor-mode busybee-theme bubbleberry-theme browse-at-remote birds-of-paradise-plus-theme badwolf-theme auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile apropospriate-theme anti-zenburn-theme ample-zen-theme ample-theme alect-themes aggressive-indent afternoon-theme ace-pinyin ace-link ace-jump-helm-line ac-ispell)))
+  )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
